f7c29bdf1c5dcefc8fbc9c3132c5505c3e33833f
duflynn <fengshuendu@hotmail.com>
2015-12-15 23:03:48 +0800

    django example

---
+++ testproject/test.py
@@ -0,0 +1,10 @@
+import random
+
+def getInt(msg):
+  return random.randint(1, 10)
+
+print(getInt(''))
+
+print(random.randint())
+
+
---
+++ testproject/untitled.py
@@ -0,0 +1,6 @@
+import random 
+
+def getInt():
+	return random.randint(1,6)
+
+print(random.)
